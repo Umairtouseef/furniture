7dad54c5b5fe6a6c0073062370354d99781ea531
Umairtouseef <umairtuseef41@gmail.com>
2025-03-03 23:17:27 +0500

    new

--- src/components/Home/HeroSection.jsx
+++ src/components/Home/HeroSection.jsx
@@ -6,15 +6,19 @@ import ProductList from "../Home/ProductList";
 import BlogSection from "../Home/BlogSection";
 import Footer from "./FooterSection";
 import ThreeSectionGrid from "./SaleOff";
+import { Link } from "react-router-dom";
 
 const HeroSection = () => {
   return (
     <>
+    <Link to="/products">
       <img
         src={backgroundbanner}
         alt="Background Banner"
         className="w-full object-cover cursor-pointer"
       />
+    </Link>
+
       <WelcomeSection />
       <ThreeSectionGrid />
       <ProductList />
