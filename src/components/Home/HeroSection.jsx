5d2ca47f12887834605c7c62e313a4057b208a4a
Umairtouseef <umairtuseef41@gmail.com>
2025-03-04 16:46:16 +0500

    changes

--- src/components/Home/HeroSection.jsx
+++ src/components/Home/HeroSection.jsx
@@ -1,28 +1,18 @@
 import React from "react";
 import backgroundbanner from "/assets/mainbanner.png";
-import WelcomeSection from "../Home/WelcomeSection";
-import FeaturesSection from "./SaleOff";
-import ProductList from "../Home/ProductList";
-import BlogSection from "../Home/BlogSection";
-import Footer from "./FooterSection";
-import ThreeSectionGrid from "./SaleOff";
+
 import { Link } from "react-router-dom";
 
 const HeroSection = () => {
   return (
     <>
-    <Link to="/products">
-      <img
-        src={backgroundbanner}
-        alt="Background Banner"
-        className="w-full object-cover cursor-pointer"
-      />
-    </Link>
-
-      <WelcomeSection />
-      <ThreeSectionGrid />
-      <ProductList />
-      <BlogSection />
+      <Link to="/products">
+        <img
+          src={backgroundbanner}
+          alt="Background Banner"
+          className="w-full object-cover cursor-pointer"
+        />
+      </Link>
     </>
   );
 };
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,13 +1,20 @@
 import HeroSection from "./HeroSection";
-import BlogSection from "./BlogSection";
-// import ShopSection from "./ShopSection";
 import Layout from "../Layout";
+import WelcomeSection from "../Home/WelcomeSection";
+import FeaturesSection from "./SaleOff";
+import ProductList from "../Home/ProductList";
+import BlogSection from "../Home/BlogSection";
 import Footer from "./FooterSection";
+import ThreeSectionGrid from "./SaleOff";
 
 function HomePage() {
   return (
     <Layout>
       <HeroSection />
+      <WelcomeSection />
+      <ThreeSectionGrid />
+      <ProductList />
+      <BlogSection />
     </Layout>
   );
 }
